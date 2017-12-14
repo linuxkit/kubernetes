d91deb83a3e9dfec775eeca21e45032b54d79b36
Robin Winkelewski <w9ncontact@gmail.com>
2017-12-14 04:32:19 +0100

    refactor cni conf binding
    Signed-off-by: Robin Winkelewski <w9ncontact@gmail.com>

--- pkg/kubelet/kubelet.sh
+++ pkg/kubelet/kubelet.sh
@@ -21,9 +21,9 @@ if [ ! -e /var/lib/cni/.opt.defaults-extracted ] ; then
     touch /var/lib/cni/.opt.defaults-extracted
 fi
 
-if [ ! -e /var/lib/cni/.cni.configs-extracted ] && [ -d /var/config/cni/etc/net.d ] ; then
-    mkdir -p /var/lib/cni/etc/net.d
-    cp /var/config/cni/etc/net.d/* /var/lib/cni/etc/net.d/
+if [ ! -e /var/lib/cni/.cni.conf-extracted ] && [ -d /var/config/cni ] ; then
+    mkdir -p /var/lib/cni/conf
+    cp /var/config/cni/* /var/lib/cni/conf/
     touch /var/lib/cni/.cni.configs-extracted
 fi
 
@@ -72,7 +72,7 @@ exec kubelet --kubeconfig=/etc/kubernetes/kubelet.conf \
 	      --cgroups-per-qos=false \
 	      --enforce-node-allocatable= \
 	      --network-plugin=cni \
-	      --cni-conf-dir=/var/lib/cni/etc/net.d \
+	      --cni-conf-dir=/etc/cni/net.d \
 	      --cni-bin-dir=/var/lib/cni/opt/bin \
 	      --cadvisor-port=0 \
 	      $KUBELET_ARGS $@
